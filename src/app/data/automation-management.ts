cb5c8e8619b21056b10309385765cf517c07fd85
dev-marceloborba <borba.marcelo@icloud.com>
2022-01-19 10:59:57 -0300

    add energy automation data

---
+++ src/app/data/automation-management.ts
@@ -0,0 +1,52 @@
+export const automationManagement = [
+    {
+        group: "Energia",
+        equipments: [
+            {
+                name: "RPP XYZ",
+                building: "Data Hall 1",
+                floor: "Andar 2",
+                room: "Cage 1",
+                status: "Online",
+                alarms: 3,
+                createdAt: "21/01/2022"
+             },
+             {
+                name: "PDU XYZ",
+                building: "Data Hall 1",
+                floor: "Andar 1",
+                room: "Sala PDU A",
+                status: "Online",
+                alarms: 0,
+                createdAt: "21/01/2022"
+             },
+             {
+                name: "QGBT A",
+                building: "Data Hall 1",
+                floor: "Terreo",
+                room: "Sala QGBT A",
+                status: "Online",
+                alarms: 0,
+                createdAt: "21/01/2022"
+             },
+             {
+                name: "QGBT B",
+                building: "Data Hall 1",
+                floor: "Terreo",
+                room: "Sala QGBT B",
+                status: "Online",
+                alarms: 0,
+                createdAt: "21/01/2022"
+             },
+             {
+                name: "QGBT C",
+                building: "Data Hall 1",
+                floor: "Terreo",
+                room: "Sala QGBT C",
+                status: "Online",
+                alarms: 0,
+                createdAt: "21/01/2022"
+             }
+        ]
+    }
+]
--- src/features/automation/pages/AutomationRegisterManagement.tsx
+++ src/features/automation/pages/AutomationRegisterManagement.tsx
@@ -1,47 +1,52 @@
 import React from "react";
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import Divider from '@mui/material/Divider'
+import Divider from "@mui/material/Divider";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 import PageTitle from "app/components/PageTitle";
 import EtcFilters from "../components/EtcFilters";
 import BuildingDropdown from "../components/BuildingDropdown";
 import FloorDropdown from "../components/FloorDropdown";
 
-
 // icons
-import AddIcon from '@mui/icons-material/Add';
-import CallSplitIcon from '@mui/icons-material/CallSplit';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-import StoreMallDirectoryIcon from '@mui/icons-material/StoreMallDirectory';
+import AddIcon from "@mui/icons-material/Add";
+import CallSplitIcon from "@mui/icons-material/CallSplit";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
 
 const AutomationRegisterManagement: React.FC = () => {
   return (
     <Container maxWidth="xl">
       <Toolbar />
       <PageTitle>Cadastros de automação</PageTitle>
-      <Box sx={{display: 'flex', alignItems: 'center'}}>
+      <Box sx={{ display: "flex", alignItems: "center", justifyContent: 'space-between', mt: 2 }}>
         <Typography variant="h6">Equipamentos de Energia</Typography>
         <EtcFilters />
       </Box>
 
-      <Box sx={{mt: 2, maxWidth: '480px'}}>
+      <Box sx={{ mt: 2, display: 'flex', maxWidth: "480px" }}>
         <BuildingDropdown />
-        <FloorDropdown />
+        <FloorDropdown sx={{ml: 2}}/>
       </Box>
 
-      <Divider />
+      <Divider sx={{mt: 2}} />
 
-      <Box sx={{display: 'flex'}}>
-        <Button variant="text" startIcon={<AddIcon />}>Novo equipamento</Button>
-        <Button variant="text" startIcon={<CallSplitIcon />}>Nova conexão</Button>
-        <Button variant="text" startIcon={<AccountBalanceIcon />}>Novo prédio</Button>
-        <Button variant="text" startIcon={<StoreMallDirectoryIcon />}>Nova sala</Button>
+      <Box sx={{ display: "flex", mt: 2 }}>
+        <Button variant="text" startIcon={<AddIcon />}>
+          Novo equipamento
+        </Button>
+        <Button variant="text" startIcon={<CallSplitIcon />}>
+          Nova conexão
+        </Button>
+        <Button variant="text" startIcon={<AccountBalanceIcon />}>
+          Novo prédio
+        </Button>
+        <Button variant="text" startIcon={<StoreMallDirectoryIcon />}>
+          Nova sala
+        </Button>
       </Box>
-
-
     </Container>
   );
 };
