92905a17a9ff90513178dd7c4d16974a0983a4a3
dev-marceloborba <borba.marcelo@icloud.com>
2022-01-20 15:22:17 -0300

    fixes on new energy equipment

--- src/features/automation/pages/NewEnergyEquipment.tsx
+++ src/features/automation/pages/NewEnergyEquipment.tsx
@@ -14,6 +14,12 @@ import { EnergyEquipmentRequest } from "app/services/automation-register";
 import PageTitle from "app/components/PageTitle";
 import Table from "app/hooks/useTable";
 import { automationParameters } from "app/data/automation-parameters";
+import EtcFilters from "../components/EtcFilters";
+
+//icons
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import SmsFailedIcon from "@mui/icons-material/SmsFailed";
+import CallSplitIcon from "@mui/icons-material/CallSplit";
 
 const NewEnergyEquipment: React.FC = () => {
   const columns = [
@@ -28,35 +34,35 @@ const NewEnergyEquipment: React.FC = () => {
       align: "right",
     },
     {
-        name: "controller",
-        label: "Controlador",
-        align: "right",
-      },
-      {
-        name: "address",
-        label: "Endereço",
-        align: "right",
-      },
-      {
-        name: "minLimit",
-        label: "Limit min",
-        align: "right",
-      },
-      {
-        name: "maxLimit",
-        label: "Limite max",
-        align: "right",
-      },
-      {
-        name: "scale",
-        label: "Escala",
-        align: "right",
-      },
-      {
-        name: "rules",
-        label: "Regras",
-        align: "right",
-      },
+      name: "controller",
+      label: "Controlador",
+      align: "right",
+    },
+    {
+      name: "address",
+      label: "Endereço",
+      align: "right",
+    },
+    {
+      name: "minLimit",
+      label: "Limit min",
+      align: "right",
+    },
+    {
+      name: "maxLimit",
+      label: "Limite max",
+      align: "right",
+    },
+    {
+      name: "scale",
+      label: "Escala",
+      align: "right",
+    },
+    {
+      name: "rules",
+      label: "Regras",
+      align: "right",
+    },
   ];
 
   const methods = useForm<EnergyEquipmentRequest>({
@@ -73,6 +79,24 @@ const NewEnergyEquipment: React.FC = () => {
       <Toolbar />
       <PageTitle>Cadastros de automação</PageTitle>
       <Box
+        sx={{ display: "flex", justifyContent: "space-between", mb: 2, mt: 1 }}
+      >
+        <Typography variant="h5">Novo equipamento de energia</Typography>
+        <EtcFilters />
+      </Box>
+      <Divider sx={{ mb: 3 }} />
+      <Box sx={{ display: "flex", mb: 2 }}>
+        <Button startIcon={<ThermostatIcon />} variant="text">
+          Novo parâmetro
+        </Button>
+        <Button startIcon={<SmsFailedIcon />} variant="text">
+          Novo alarme
+        </Button>
+        <Button startIcon={<CallSplitIcon />} variant="text">
+          Nova conexão
+        </Button>
+      </Box>
+      <Box
         component="form"
         noValidate
         autoComplete="off"
@@ -81,24 +105,53 @@ const NewEnergyEquipment: React.FC = () => {
         <FormProvider {...methods}>
           <Grid container spacing={2}>
             <Grid item md={6}>
-              <Typography variant="h4">Dados do equipamento</Typography>
-              <ControlledTextInput name="name" label="Nomenclatura" />
+              <Typography variant="h6" sx={{ mb: 1 }}>
+                Dados do equipamento
+              </Typography>
+              <ControlledTextInput
+                sx={{ maxWidth: "320px" }}
+                name="name"
+                label="Nomenclatura"
+              />
             </Grid>
-
             <Grid item md={6}>
-              <Typography variant="h4">Local</Typography>
-              <ControlledTextInput name="campus" label="Campus" />
-              <ControlledTextInput name="floor" label="Andar" />
-              <ControlledTextInput name="building" label="Prédio" />
-              <ControlledTextInput name="room" label="Sala" />
+              <Typography variant="h6" sx={{ mb: 1 }}>
+                Local
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item md={6}>
+                  <ControlledTextInput name="campus" label="Campus" />
+                </Grid>
+                <Grid item md={6}>
+                  <ControlledTextInput name="floor" label="Andar" />
+                </Grid>
+                <Grid item md={6}>
+                  <ControlledTextInput name="building" label="Prédio" />
+                </Grid>
+                <Grid item md={6}>
+                  <ControlledTextInput name="room" label="Sala" />
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </FormProvider>
-        <Button type="submit" variant="contained">
-          Salvar
-        </Button>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            size="medium"
+            sx={{ mt: 2 }}
+            type="submit"
+            variant="contained"
+          >
+            Salvar
+          </Button>
+        </Box>
       </Box>
-
       <Box sx={{ mt: 2 }}>
         <Typography variant="h4">Parâmetros</Typography>
         <Table columns={columns} rows={automationParameters} />
@@ -114,5 +167,5 @@ const validationSchema: SchemaOf<EnergyEquipmentRequest> = object().shape({
   campus: string().required("Campus é obrigatório"),
   floor: string().required("Andar é obrigatório"),
   building: string().required("Prédio é obrigatório"),
-  room: string().required("Andar é obrigatório"),
+  room: string().required("Sala é obrigatória"),
 });
