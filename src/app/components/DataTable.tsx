22e69ec594556f4332de57a8a7d349d44422611c
dev-marceloborba <borba.marcelo@icloud.com>
2022-04-28 11:00:09 -0300

    feat: add previous items on datatable

---
+++ src/app/components/DataTable.tsx
@@ -0,0 +1,352 @@
+import React, { useState, useEffect } from "react";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
+
+interface DataTableProps {
+  columns: Column[];
+  rows: any[];
+  title: string;
+  options: {
+    selectedItems: (items: any[]) => void;
+    previousItems?: any[];
+  };
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface Column {
+  name: string;
+  label: string;
+}
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+  columns: Column[];
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+    columns,
+  } = props;
+  const createSortHandler =
+    (property: string) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts",
+            }}
+          />
+        </TableCell>
+        {columns.map((column, index) => (
+          <TableCell
+            key={column.name}
+            align={index === 0 ? "left" : "right"}
+            padding={"normal"}
+            sortDirection={orderBy === column.name ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === column.name}
+              direction={orderBy === column.name ? order : "asc"}
+              onClick={createSortHandler(column.name)}
+            >
+              {column.label}
+              {orderBy === column.name ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+  title: string;
+}
+
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const { numSelected, title } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selecionados
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          {title}
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+const DataTable: React.FC<DataTableProps> = ({
+  rows,
+  columns,
+  title,
+  options,
+}) => {
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState(columns[0].name);
+  const [selected, setSelected] = useState<any[]>(options.previousItems || []);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: string
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, row: any) => {
+    const selectedIndex = selected.indexOf(row);
+    let newSelected: any[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, row);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (row: any) => selected.indexOf(row) !== -1;
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  useEffect(() => {
+    options.selectedItems(selected);
+  }, [options, selected]);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} title={title} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={"medium"}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+              columns={columns}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={index}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId,
+                          }}
+                        />
+                      </TableCell>
+                      {columns.map((column, index) => {
+                        return (
+                          <TableCell
+                            key={index}
+                            align={index === 0 ? "left" : "right"}
+                          >
+                            {row[column.name]}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows?.length ?? 0}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+};
+
+export default DataTable;
--- src/features/datacenter/components/ParameterSelection.tsx
+++ src/features/datacenter/components/ParameterSelection.tsx
@@ -5,15 +5,8 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { useListAllParametersQuery } from "app/services/datacenter";
 import Loading from "app/components/Loading";
 
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableContainer from "@mui/material/TableContainer";
-import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TablePagination from "@mui/material/TablePagination";
-import Checkbox from "@mui/material/Checkbox";
 import { EquipmentParameterResponse } from "app/models/data-center.model";
+import DataTable from "app/components/DataTable";
 
 type ParameterSelectionProps = {
   open: boolean;
@@ -29,8 +22,6 @@ const ParameterSelection: React.FC<ParameterSelectionProps> = ({
   previousParameters,
 }) => {
   const { data: parameters, isLoading } = useListAllParametersQuery();
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [page, setPage] = useState(0);
   const [selected, setSelected] = useState<EquipmentParameterResponse[]>([]);
 
   useEffect(() => {
@@ -51,125 +42,14 @@ const ParameterSelection: React.FC<ParameterSelectionProps> = ({
     handleCloseModal();
   };
 
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelecteds = parameters?.map((n) => n)!;
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (
-    event: React.MouseEvent<unknown>,
-    row: EquipmentParameterResponse
-  ) => {
-    const selectedIndex = selected.indexOf(row);
-    let newSelected: EquipmentParameterResponse[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, row);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-    setSelected(newSelected);
-  };
-
-  const isSelected = (row: EquipmentParameterResponse) =>
-    selected.indexOf(row) !== -1;
-
-  const emptyRows =
-    page > 0
-      ? Math.max(0, (1 + page) * rowsPerPage - (parameters?.length || 0))
-      : 0;
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
   return (
     <Dialog open={open} onClose={handleCloseModal}>
       <DialogTitle>Selecionar parâmetro</DialogTitle>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow
-              sx={(theme) => ({
-                backgroundColor: theme.palette.background.paper,
-              })}
-            >
-              <TableCell padding="checkbox">
-                <Checkbox color="primary" onChange={handleSelectAllClick} />
-              </TableCell>
-              <TableCell align="left">Nome</TableCell>
-              <TableCell align="right">Unidade</TableCell>
-              <TableCell align="right">Limite mínimo</TableCell>
-              <TableCell align="right">Limite máximo</TableCell>
-              <TableCell align="right">Escala</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody
-            sx={(theme) => ({
-              " &  .MuiTableRow-root:hover": {
-                backgroundColor: theme.palette.background.paper,
-              },
-            })}
-          >
-            {parameters
-              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((parameter) => {
-                const isItemSelected = isSelected(parameter);
-                return (
-                  <TableRow
-                    key={parameter.id}
-                    onClick={(event) => handleClick(event, parameter)}
-                    selected={isItemSelected}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox color="primary" checked={isItemSelected} />
-                    </TableCell>
-                    <TableCell align="left">{parameter.name}</TableCell>
-                    <TableCell align="right">{parameter.unit}</TableCell>
-                    <TableCell align="right">{parameter.lowLimit}</TableCell>
-                    <TableCell align="right">{parameter.highLimit}</TableCell>
-                    <TableCell align="right">{parameter.scale}</TableCell>
-                  </TableRow>
-                );
-              })}
-            {emptyRows > 0 && (
-              <TableRow
-                style={{
-                  height: 53 * emptyRows,
-                }}
-              >
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={parameters?.length ?? 0}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+      <DataTable
+        rows={parameters ?? []}
+        columns={columns}
+        title="Parâmetros"
+        options={{ selectedItems: setSelected, previousItems: selected }}
       />
       <Button variant="contained" onClick={handleSaveSelection}>
         Selecionar
@@ -178,5 +58,28 @@ const ParameterSelection: React.FC<ParameterSelectionProps> = ({
     </Dialog>
   );
 };
+
+const columns = [
+  {
+    label: "Nome",
+    name: "name",
+  },
+  {
+    label: "Unidade",
+    name: "unit",
+  },
+  {
+    label: "Limite mínimo",
+    name: "lowLimit",
+  },
+  {
+    label: "Limite máximo",
+    name: "highLimit",
+  },
+  {
+    label: "Escala",
+    name: "scale",
+  },
+];
 
 export default ParameterSelection;
