037bc4a3390f7802b5238ff91600900dc7ca8920
dev-marceloborba <borba.marcelo@icloud.com>
2022-05-02 15:53:41 -0300

    fix: ui form elements on modbus tag

--- src/features/automation/NewModbusTag.tsx
+++ src/features/automation/NewModbusTag.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { useAddModbusTagMutation } from "app/services/automation";
@@ -11,31 +8,33 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { SchemaOf, object, string, number } from "yup";
 import ControlledTextInput from "app/components/ControlledTextInput";
 import { ModbusTagRequest } from "app/models/automation.model";
+import Form from "app/components/Form";
+import SubmitButton from "app/components/SubmitButton";
+import Loading from "app/components/Loading";
+import { useToast } from "app/components/Toast";
 
 const NewModbusTag: React.FC = () => {
   const methods = useForm<ModbusTagRequest>({
     resolver: yupResolver(validationSchema),
   });
-  const [addModbusTag, { isLoading, isError }] = useAddModbusTagMutation();
+  const [addModbusTag, { isLoading }] = useAddModbusTagMutation();
+  const toast = useToast();
 
   const { handleSubmit } = methods;
 
   const onSubmit: SubmitHandler<ModbusTagRequest> = async (data) => {
     try {
       const result = await addModbusTag(data).unwrap();
-      console.log(result);
+      toast.open(`Tag ${result.data.name} criado com sucesso`, 2000, "success");
     } catch (error) {
-      console.log(error);
+      toast.open(`Erro ao criar tag: ${error}`, 2000, "error");
     }
   };
 
   return (
     <Container maxWidth="xs">
       <Typography variant="h5">Criar Tag Modbus</Typography>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
+      <Form
         onSubmit={handleSubmit(onSubmit)}
         sx={{ maxWidth: "640px", "& .MuiTextField-root": { mt: 2 } }}
       >
@@ -46,17 +45,10 @@ const NewModbusTag: React.FC = () => {
           <ControlledTextInput name="address" label="Endereço" />
           <ControlledTextInput name="size" label="Tamanho" />
           <ControlledTextInput name="deadband" label="Banda morta" />
-          <Button
-            sx={{ mt: 2 }}
-            fullWidth
-            type="submit"
-            color="primary"
-            variant="contained"
-          >
-            Criar
-          </Button>
+          <SubmitButton sx={{ mt: 2 }} fullWidth label="Criar" />
         </FormProvider>
-      </Box>
+      </Form>
+      <Loading open={isLoading} />
     </Container>
   );
 };
