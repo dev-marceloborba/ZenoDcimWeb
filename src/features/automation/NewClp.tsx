b40bcfe68bdd7c6c1538855792eab725c234682a
dev-marceloborba <borba.marcelo@icloud.com>
2022-05-02 15:50:24 -0300

    fix: ui form elements on clp

--- src/features/automation/NewClp.tsx
+++ src/features/automation/NewClp.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import ControlledTextInput from "app/components/ControlledTextInput";
@@ -10,31 +8,33 @@ import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { SchemaOf, object, number, string } from "yup";
 import { PlcRequest } from "app/models/automation.model";
+import Form from "app/components/Form";
+import { useToast } from "app/components/Toast";
+import Loading from "app/components/Loading";
+import SubmitButton from "app/components/SubmitButton";
 
 const NewClp: React.FC = () => {
   const methods = useForm<PlcRequest>({
     resolver: yupResolver(validationSchema),
   });
-  const [mutation, { isLoading, isError }] = useAddPlcMutation();
+  const [mutation, { isLoading }] = useAddPlcMutation();
+  const toast = useToast();
 
   const { handleSubmit } = methods;
 
   const onSubmit: SubmitHandler<PlcRequest> = async (data) => {
     try {
       const result = await mutation(data).unwrap();
-      console.log(result);
+      toast.open(`CLP ${result.data.name} criado com sucesso`, 2000, "success");
     } catch (error) {
-      console.log(error);
+      toast.open(`Erro ao criar CLP: ${error}`, 2000, "error");
     }
   };
 
   return (
     <Container maxWidth="xs">
       <Typography variant="h5">Criar PLC</Typography>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
+      <Form
         onSubmit={handleSubmit(onSubmit)}
         sx={{ maxWidth: "640px", "& .MuiTextField-root": { mt: 2 } }}
       >
@@ -44,17 +44,10 @@ const NewClp: React.FC = () => {
           <ControlledTextInput name="ipAddress" label="Endereço IP" />
           <ControlledTextInput name="tcpPort" label="Porta TCP/IP" />
           <ControlledTextInput name="scan" label="Varredura" />
-          <Button
-            sx={{ mt: 2 }}
-            fullWidth
-            type="submit"
-            color="primary"
-            variant="contained"
-          >
-            Criar
-          </Button>
+          <SubmitButton sx={{ mt: 2 }} label="Criar" fullWidth />
         </FormProvider>
-      </Box>
+      </Form>
+      <Loading open={isLoading} />
     </Container>
   );
 };
